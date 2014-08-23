763d8841451c85442ff5561805255b13f7f07e37
Ricky Ng <dummey@gmail.com>
2014-08-23 02:19:13 -0500

    Magical python counting of words used.

--- rng-parsing/parser.py
+++ rng-parsing/parser.py
@@ -5,6 +5,8 @@ from nltk.corpus import stopwords
 from os import listdir
 from os.path import isfile, join
 
+from collections import Counter
+
 def get_core_words( text ):
 
     #TOKENIZATION
@@ -38,8 +40,13 @@ def pre_process_text ( text ):
 root_path = "/home/dummey/global-hack-data/articles"
 text_file_paths = [ join(root_path, f) for f in listdir(root_path) if isfile(join(root_path,f)) ]
 
+aggregate = Counter();
+
 for text_file_path in text_file_paths:
     raw_text = pre_process_text(load_text(text_file_path))
     core_words = get_core_words(raw_text)
-    print core_words
-    break
+    aggregate.update( Counter(core_words) )
+    if len(list(aggregate.elements())) > 10000:
+        break
+
+print aggregate.most_common(100)
