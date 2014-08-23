11360185f9f1e36d2541cb86185bc07d166cdfcd
Ricky Ng <dummey@gmail.com>
2014-08-23 02:07:36 -0500

    cleaned up script

--- rng-parsing/parser.py
+++ rng-parsing/parser.py
@@ -1,33 +1,45 @@
 import nltk
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords
- 
-nltk.download()
 
-#TOKENIZATION
- 
-sentence = "Operating system is the heart of a computer"
-b = nltk.word_tokenize(sentence)
-print b
- 
-#CONVERT INTO LOWER CASE
- 
-looper = 0
-for token in b:
- b[looper] = token.lower()
- looper+=1
-print b
- 
-#REMOVE THE STOPWORDS FROM THE FILE
- 
-minlength = 2
-c = [token for token in b if (not token in stopwords.words('english')) and len(token) >= minlength]
-print c
- 
-#STEMMING THE WORDS TO ITS BASE FORM
-stemmer = SnowballStemmer("english")
-looper1 = 0
-for token in c:
- c[looper1] = stemmer.stem(token)
- looper1 +=1
-print c
+from os import listdir
+from os.path import isfile, join
+
+def get_core_words( text ):
+
+    #TOKENIZATION
+    b = nltk.word_tokenize(text)
+
+    #CONVERT INTO LOWER CASE
+    looper = 0
+    for token in b:
+        b[looper] = token.lower()
+        looper+=1
+
+    #REMOVE THE STOPWORDS FROM THE FILE
+    minlength = 2
+    c = [token for token in b if (not token in stopwords.words('english')) and len(token) >= minlength]
+
+    #STEMMING THE WORDS TO ITS BASE FORM
+    stemmer = SnowballStemmer("english")
+    looper1 = 0
+    for token in c:
+        c[looper1] = stemmer.stem(token.decode("utf8"))
+        looper1 +=1
+    return c
+
+def load_text ( file_path ):
+    return open(file_path, 'r').read()
+
+def pre_process_text ( text ):
+    text.decode("utf8")
+    return text
+
+root_path = "/home/dummey/global-hack-data/articles"
+text_file_paths = [ join(root_path, f) for f in listdir(root_path) if isfile(join(root_path,f)) ]
+
+for text_file_path in text_file_paths:
+    raw_text = pre_process_text(load_text(text_file_path))
+    core_words = get_core_words(raw_text)
+    print core_words
+    break
