4064273f64a12bd0b6c18d675130ab9179a0618b
MoMenne <mpmenne@electronicsystemssupport.com>
2014-08-24 10:45:33 -0500

    adding angular examples

---
+++ gh2frontend/html5-boilerplate-4.3.0/app.js
@@ -0,0 +1,37 @@
+console.log("app.js is loaded")
+var launchCodeTvApp = angular.module('application', [])
+
+launchCodeTvApp.controller('OntologyController', function ($scope, $http) {
+  var app = this;
+  $scope.message = "yo yo yo!";
+  $scope.bricks = []
+  $scope.bricks.push({"_id":"1", "src":"http://www.thewholeheartedlife.com/blog/wp-content/uploads/2012/01/baby_penguin.jpg"})
+  $scope.bricks.push({"_id":"2", "src":"http://www.thewholeheartedlife.com/blog/wp-content/uploads/2012/01/baby_penguin.jpg"})
+
+
+  $scope.hide = function(square) {
+    _.remove($scope.bricks, function(current) { return current._id === square.brick._id });
+  }
+
+  $scope.more = function() {
+    $http.get($scope.nextPage).success(function(data) {
+      $scope.nextPage = data._links.next.href
+      for (var i = 0; i < data._items.length; i++ ){
+        $scope.bricks.push(data._items[i]);
+      }
+    }).error(function(data) {
+      alert('Error!!!!')
+    })
+  }
+
+  $http.get('api/v1/nodes').success(function(data) {
+    $scope.nextPage = data._links.next.href
+    for (var i = 0; i < data._items.length; i++ ){
+      $scope.bricks.push(data._items[i]);
+    }
+
+    console.log('lets stop here')
+  }).error(function(data) {
+    alert('Error!!!!')
+  })
+})
