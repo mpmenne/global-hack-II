8637705aa126d5ab66400588d2f1c639f2c847d3
Tim <timking@outlook.com>
2014-08-23 21:58:59 -0500

    fixing settings

--- gh2backend/wsgi_app.py
+++ gh2backend/wsgi_app.py
@@ -10,5 +10,15 @@ app = Eve(settings=os.path.join(
 def hello_world():
     return 'Hello World!'
 
+
+def add_score(items):
+    connections = app.data.driver.db['connections']
+    connection = connections.find_one({'related_connection_id': items[0]['related_connection_id']})
+    # account = connections.insert(docs)
+    print connection
+
+app = Eve()
+app.on_insert_transactions += add_score
+
 if __name__ == '__main__':
     app.run('0.0.0.0')
